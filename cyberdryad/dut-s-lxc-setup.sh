fcc734a7b272584d891d44f8ad9f287a2178b0f2
Karol Lewandowski <k.lewandowsk@samsung.com>
2018-07-25 15:58:29 +0200

    Add DUT Supervisor setup scripts
    This commit introduces basic, proof-of-concept set of scripts
    that setup:
    
     lxc - to serve as DUT-S - Supervisor
    
     qemu inside lxc - to serve as DUT - the Device Under Test
    
     dryad - the Boruta Server Agent
    
    Change-Id: I415dbbf89330ffbf64c9bf7c5e9552eb70a62005
    Signed-off-by: Karol Lewandowski <k.lewandowsk@samsung.com>
    [ Multiple fixes, including:
      - add UUID generation
      - add group stm
      - add libsystemd-dev necessary for building stm
      - add installation of stm with -dummy flag
      - replace manual building of Go binaries with installation of
        byproducts of `go get ./...` ]
    Authored by Michał Sidor <m.sidor@samsung.com>

---
+++ cyberdryad/dut-s-lxc-setup.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# Setup LXC container to serve as DUT supervisor
+#
+# See README.dut-s for help.
+
+set -ex
+
+VM_NAME=${DUT_VM_NAME:-dut-s}
+BORUTA_ADDRESS=${BORUTA_ADDRESS:-127.0.0.1}
+BORUTA_PORT=${BORUTA_PORT:-7175}
+UUID=$(uuidgen -r)
+
+lxc_install() {
+    lxc-destroy -f -n "$VM_NAME" || true
+    lxc-create -n "$VM_NAME" -t ubuntu -- -r xenial --packages=virtinst,libvirt-bin,libvirt-daemon,qemu-kvm,qemu-utils,openssh-server,wget,expect,git,libsystemd-dev
+    # print big scarry warning if network is not setup correctly
+    if ! grep -q 'lxc.network.type.*veth' "/var/lib/lxc/$VM_NAME/config"; then
+        echo "@@@ warning: this script assumes lxc uses veth<->lxcbr0 networking setup BY DEFAULT @@@"
+        echo "if you are using different distro than ubuntu please ensure lxc.network.* is set up correctly"
+    fi
+}
+
+tools_install()
+{
+    git archive HEAD | lxc-attach -n "$VM_NAME" -- tar xvfC - "$1/bin"
+}
+
+tools_setup()
+{
+    cat <<EOF | lxc-attach -n "$VM_NAME" dd of=/var/tmp/dut-s-setup.env
+DESTDIR=${DESTDIR}
+BORUTA_ADDRESS=${BORUTA_ADDRESS}
+BORUTA_PORT=${BORUTA_PORT}
+UUID=${UUID}
+EOF
+    lxc-attach -n "$VM_NAME" -- "dut-s-reconfigure.sh"
+}
+
+DESTDIR="/usr/local"
+
+lxc_install
+
+lxc-start -n "$VM_NAME"
+# Hack: wait a while for container to boot and bring up networking - needed for tools setup
+sleep 5
+tools_install "$DESTDIR"
+tools_setup
+
+lxc-stop -r -n "$VM_NAME"
+
+echo "DUT-supervisor successfully created as lxc $VM_NAME container"
---
+++ cyberdryad/dut-s-reconfigure.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+set -ex
+
+export GO=/usr/lib/go-1.10/bin/go
+
+ARCH="$(uname -m)"
+UNITDIR="/etc/systemd/system"
+UNIT2ENABLE=""
+
+apt_setup()
+{
+    echo 'deb http://archive.ubuntu.com/ubuntu xenial-backports main restricted universe multiverse' > /etc/apt/sources.list.d/dut-s-xenial-backports.list
+    apt-get update
+    apt-get install -y golang-1.10-go
+}
+
+users_setup()
+{
+    getent passwd boruta-user || adduser --shell /bin/bash boruta-user </dev/null
+    getent group kvm | grep -q boruta-user || adduser boruta-user kvm
+    getent group libvirtd | grep -q boruta-user || adduser boruta-user libvirtd
+    getent group stm || addgroup --system stm
+    getent group stm | grep -q boruta-user || adduser boruta-user stm
+}
+
+dryad_setup()
+{
+    mkdir -p /etc/boruta
+    # dryad has to fail in first run AND create configuration template
+    # we run it here for the template
+    dryad || true
+    sed -i \
+        -e 's,boruta_address =.*,boruta_address = "'${BORUTA_ADDRESS}:${BORUTA_PORT}'",' \
+        -e "/\[caps\]/ a device_type = \"qemu\"\narchitecture = \"$ARCH\"\nUUID = \"$UUID\"" \
+        /etc/boruta/dryad.conf
+}
+
+stm_unit_install()
+{
+    muxpi_path="$1"
+    install -m0644 "${muxpi_path}/sw/nanopi/stm.service" "$UNITDIR/stm.service"
+    sed -i -e 's/-serve/-serve -dummy/' "$UNITDIR/stm.service"
+    install -m0644 "${muxpi_path}/sw/nanopi/stm.socket" "$UNITDIR/stm.socket"
+    install -m0644 "${muxpi_path}/sw/nanopi/stm-user.socket" "$UNITDIR/stm-user.socket"
+    sed -i -e "s,/usr/bin,$DESTDIR/bin,g" -e "s,WantedBy=sockets.target,WantedBy=basic.target," "$UNITDIR/stm.service" "$UNITDIR/stm.socket" "$UNITDIR/stm-user.socket"
+
+    UNITS2ENABLE="$UNITS2ENABLE stm.socket stm-user.socket"
+}
+
+dryad_unit_install()
+{
+    cat > "$UNITDIR/dryad.service" <<EOF
+[Unit]
+Description=SLAV Dryad - Boruta Server Agent
+Requires=stm.socket
+
+[Service]
+Type=simple
+ExecStart=$DESTDIR/bin/dryad
+Restart=always
+
+[Install]
+WantedBy=default.target
+EOF
+    UNITS2ENABLE="$UNITS2ENABLE dryad.service"
+}
+
+systemd_unit_enable()
+{
+    systemctl daemon-reload
+    systemctl enable $UNITS2ENABLE
+}
+
+slav_setup()
+{
+    HOST=git.tizen.org
+    D=/src/slav
+    SRC="$D/src/$HOST"
+
+    mkdir -p "$D/pkg" "$SRC/tools"
+
+    for i in tools/boruta tools/weles tools/muxpi; do
+        # assume previous invocation might have failed - better to fetch again
+        test -d "$SRC/$i" && rm -rf "$SRC/$i"
+        git clone "git://$HOST/$i" "$SRC/$i"
+    done
+
+    export GOPATH="$D"
+    (cd "$D" && $GO get ./...)
+    install -m0755 -t "$DESTDIR/bin" "$GOPATH/bin/stm" "$GOPATH/bin/dryad"
+
+    dryad_setup
+    dryad_unit_install
+    stm_unit_install "$SRC/tools/muxpi"
+    systemd_unit_enable
+}
+
+# This script assumes DESTDIR, UUID, BORUTA_ADDRESS & BORUTA_PORT
+# is defined in the environment.  In normal setup it's passed from
+# dut-s-lxc-setup via following file:
+. /var/tmp/dut-s-setup.env
+
+apt_setup
+users_setup
+slav_setup
