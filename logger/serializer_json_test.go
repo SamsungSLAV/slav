4fc9262995c9cf825977c9ae1ace4793ae7a8c21
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2018-07-26 17:19:29 +0200

    Implement Serializer for JSON
    SerializerJSON converts Entry structure into JSON format stored in raw
    byte buffer.
    
    The structure for the entry consist of 4 top level fields:
      level      - log message importance level as string
      message    - log message passed to logging function
      timestamp  - timestamp in customizable format
      properties - all Entry's properties as map serialized to JSON
                   creating lower level of the structure
    
    For timestamp format customization, set SerializerJSON.TimestampFormat
    to one of the values accepted by the time.Format.
    The default value is time.RFC3339.
    
    If Entry does not contain any properties, the properties field is omitted.
    
    Change-Id: I3a72a0d82b404a819543cc264f89616052bfd340
    Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>

---
+++ logger/serializer_json_test.go
@@ -0,0 +1,108 @@
+/*
+ *  Copyright (c) 2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+package logger
+
+import (
+	"errors"
+	"math"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("SerializerJSON", func() {
+	var (
+		e *Entry
+		s *SerializerJSON
+	)
+
+	BeforeEach(func() {
+		e = &Entry{
+			Level:     ErrLevel,
+			Message:   "message",
+			Timestamp: time.Unix(1234567890, 0),
+		}
+		s = NewSerializerJSON()
+	})
+	Describe("NewSerializerJSON", func() {
+		It("should create a new object with default configuration", func() {
+			Expect(s).NotTo(BeNil())
+			Expect(s.TimestampFormat).To(Equal(DefaultSerializerJSONTimestampFormat))
+		})
+	})
+	Describe("Serialize", func() {
+		It("should serialize simple message without properties", func() {
+			buf, err := s.Serialize(e)
+			Expect(err).NotTo(HaveOccurred())
+			expected := []byte(`{"level":"error","message":"message","timestamp":` +
+				`"2009-02-13T23:31:30Z"}`)
+			Expect(buf).To(Equal(expected))
+		})
+		It("should serialize message with properties", func() {
+			e.WithProperties(Properties{
+				"name": "Alice",
+				"male": true,
+				"age":  37,
+				"skills": Properties{
+					"cooking": "expert",
+					"coding":  7,
+				},
+				"issues": "",
+			})
+			buf, err := s.Serialize(e)
+			Expect(err).NotTo(HaveOccurred())
+			expected := []byte(`{"level":"error","message":"message","timestamp":` +
+				`"2009-02-13T23:31:30Z","properties":{"age":37,"issues":"","male":true,"name":` +
+				`"Alice","skills":{"coding":7,"cooking":"expert"}}}`)
+			Expect(buf).To(Equal(expected))
+		})
+		It("should serialize message with error", func() {
+			err := errors.New("test error")
+			e.WithError(err)
+			buf, err := s.Serialize(e)
+			Expect(err).NotTo(HaveOccurred())
+			expected := []byte(`{"level":"error","message":"message","timestamp":` +
+				`"2009-02-13T23:31:30Z","properties":{"error":"test error"}}`)
+			Expect(buf).To(Equal(expected))
+		})
+		It("should return error if serialization is not possible", func() {
+			e.WithProperties(Properties{
+				"power": math.Inf(1),
+			})
+			buf, err := s.Serialize(e)
+			Expect(err.Error()).To(Equal("json: unsupported value: +Inf"))
+			Expect(buf).To(BeNil())
+		})
+		It("should serialize with default timestamp format if format is not initialized", func() {
+			s = &SerializerJSON{}
+			buf, err := s.Serialize(e)
+			Expect(err).NotTo(HaveOccurred())
+			expected := []byte(`{"level":"error","message":"message","timestamp":` +
+				`"2009-02-13T23:31:30Z"}`)
+			Expect(buf).To(Equal(expected))
+		})
+		It("should serialize with different timestamp format", func() {
+			s.TimestampFormat = time.ANSIC
+			buf, err := s.Serialize(e)
+			Expect(err).NotTo(HaveOccurred())
+			expected := []byte(`{"level":"error","message":"message","timestamp":` +
+				`"Fri Feb 13 23:31:30 2009"}`)
+			Expect(buf).To(Equal(expected))
+		})
+	})
+})
