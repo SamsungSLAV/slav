c913798306c11f300947931d17abb6405b5c6b64
Michal Sidor <m.sidor@samsung.com>
2018-11-08 15:46:01 +0100

    Add Velen, environmental protection for Dryads
    Change-Id: If2bae1d272c60e967710b12d6b3fc3d5820e2ec5

---
+++ velen/velen-destroy.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+#include "velen-destroy.h"
+#include "detach_mount.h"
+#include "config.h"
+
+int velen_destroy() {
+  return detach_mount(VELEN_PATH "/overlay")
+    || detach_mount(VELEN_PATH "/top")
+    || EXIT_SUCCESS;
+}
