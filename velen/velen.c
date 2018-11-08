5ba2dc507e6268966d201c6ef0c9e2a2365d8070
Michal Sidor <m.sidor@samsung.com>
2018-11-08 15:46:01 +0100

    Add Velen, environmental protection for Dryads
    Change-Id: If2bae1d272c60e967710b12d6b3fc3d5820e2ec5

---
+++ velen/velen.c
@@ -0,0 +1,45 @@
+// Copyright (c) 2018-2019 Samsung Electronics Co., Ltd All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+//
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "velen-destroy.h"
+#include "velen-prepare.h"
+#include "velen-run.h"
+#include "velen-shell.h"
+
+int main(int argc, char* argv[]) {
+  if (strcmp(argv[0], "velen-prepare") == 0) {
+    return velen_prepare(&argv[1]);
+  }
+
+  if (strcmp(argv[0], "velen-run") == 0) {
+    return velen_run(argc - 1, &argv[1]);
+  }
+
+  if (strcmp(argv[0], "velen-destroy") == 0) {
+    return velen_destroy();
+  }
+
+  if (strcmp(argv[0], "velen-shell") == 0) {
+    return velen_shell(argc, argv);
+  }
+
+  fprintf(stderr, "no tool with such name found: %s\n", argv[0]);
+  return EXIT_FAILURE;
+}
