d0965f74a84c43ccaad01ac5f64840811dbf0fc3
Alexander Mazuruk <a.mazuruk@samsung.com>
2018-11-22 20:30:57 +0100

    squash&abandon me!
    @ Lukasz - as discussed instead of review, I'm providing a squashable.
    
    I had removed some (in my opinion unnecessary) information, some
    duplications etc.
    
    I have also adjusted formatting so bullet lists are not rendered as code
    (misleading)
    
    This made the doc more concise (txt-wise not lines-wise as many newlines
    were added for bullet lists)
    
    @lukasz- lmk if you will squash it or should i abandon
    
    Change-Id: I78078ffe5fb86c2a5adbc6dec990b00cf5abdb67
    Signed-off-by: Alexander Mazuruk <a.mazuruk@samsung.com>

--- logger/doc.go
+++ logger/doc.go
@@ -17,79 +17,88 @@
 /*
 Package logger provides logging mechanism for SLAV projects.
 
-It is a completely new logger package, as there we didn't find any ready one
-that meets our requirements:
-	* have syslog-like levels;
-	* do not panic with highest priority log;
-	* have structured log entities (to allow automated parsing).
+This logger was created, as we didn't find a logger fitting our requirements:
+
+* have syslog-like levels;
+
+* do not panic with highest priority log;
+
+* have structured log entities (to allow automated parsing).
 
 Basics
 
-The use of logger is pretty simple.
+This logger is really simple - it consists of:
 
-First you will need a Logger structure that will: filter, serialize and write log entities for you.
-You can create your own Logger with NewLogger function ...
-	log = logger.NewLogger()
-	log.Warning("Some warning.")
-or even easier use the default one, that is already created fo you.
+* filter - to get messages with log level above treshold
+
+* serializer - to get nice plain text or json formatted messages
+
+* writer - to write to stdout/stderr or syslog
+
+The usage of logger is really simple:
+	if data, err := someFunction(ctx); err!=nil{
+	logger.WithError(err).WithProperty("context", ctx).Warning("Some warning.")
+	}
+
+Which would yield following output:
+	[0.000093] [WAR] [filename.go:11] "Some warning" {context:someFunctionInput;}
+	^timestamp ^lvl  ^call context    ^message       ^properties
+
+Where:
+
+* Timestamp - time of log creation - set during log entity processing;
+
+* Level - level of log message entity defined by completing function;
+
+* CallContext - source code context - set during log entity processing.
+
+* Message - string part of log message entity defined by completing function;
+
+* Properties - a key-value map of log message entities;
+
+Usage
+
+The simplest use-case would be to use default logger (residing in the logger package):
+	logger.SetThreshold(logger.InfoLevel)
 	logger.Warning("Some warning.")
-Then you will be able to call Logger's methods for logging messages with proper level, e.g. Warning
-as you can see in the above examples.
 
-Logger structure can be setup by defining threshold and different backends.
-Log entities can be also more compex containing properties or information about an error.
-All of these enhanced features will be described below.
+All log message entities that are processed by Logger are checked, if they pass threshold test.  If
+Logger's threshold is set to Warning level then Debug, Info and Notice messages won't be logged,
+because their levels is lower, but Warinig, Error, Critical, Alert and Emergency logs will be
+processed fine.
 
-Levels
+You can also create your instance of logger and customize it:
+	// create new instance of logger
+	log:= logger.NewLogger()
+	// set threshold to filter out Debug logs.
+	log.SetThreshold(lvl)
+	// register custom backend.
+	log.AddBackend("myBackend", Backend{
+		Filter:     NewFilterPassAll(),
+		Serializer: NewSerializerJSON(),
+		Writer:     NewWriterFile(filename, 0444),
+	})
+	// set the logger as default.
+	logger.SetDefault(log)
 
-There are 8 log levels defined - exactly matching syslog levels:
-	* EmergLevel - used when system is unusable;
-	* AlertLevel - used when action must be taken immediately;
-	* CritLevel - used when critical conditions occur;
-	* ErrLevel - used when error conditions occur;
-	* WarningLevel - used when warning conditions occur;
-	* NoticeLevel - used when normal, but significant, conditions occur;
-	* InfoLevel - used for logging informational message;
-	* DebugLevel - used for logging debug-level message.
-To log an entity with one of these levels simply use dedicated method of Logger or existing
-log message entity, e.g.
+Logging entries
+
+There are 8 log levels defined - exactly matching syslog levels. Please refer to documentation of
+type Level for consts names.
+
+To log an entity with one of these levels simply use dedicated method of Logger.  For every log
+level there are 2 methods available: normal and formatted that can be used the same way fmt.Print
+and fmt.Printf are:
 	logger.Notice("You were notified!")
 	logger.Noticef("You were notified about %s!", "usage of logger")
-For every log level there are 2 methods available: normal and formatted that can be used
-the same way fmt.Print and fmt.Printf are.
 
 You can also use generic methods: Log, Logf which require level as the first parameter.
 
-Notice that all of functions described in this paragraph complete creation of log message entity
-and causes Logger structure to process the entity. It will be more clear after reading about
-Entry structure and creation of structured logs.
+Adding more information
 
-Threshold
-
-Every Logger structure has a Threshold value that can be set with SetThreshold method
-and read with Threshold method.
-All log message entities that are processed by Logger are verified, if they pass threshold test.
-It checks if log message level is higher than threshold, e.g.
-If Logger's threshold is set to Warning level then Debug, Info and Notice messages
-won't be logged, because their levels is lower, but Warinig, Error, Critical, Alert
-and Emergency logs will be processed fine.
-
-Entry
-
-Every log message entity is kept in Entry structure.
-It contains following elements:
-	* Level - level of log message entity defined by completing function;
-	* Message - string part of log message entity defined by completing function;
-	* Properties - a key-value map of log message entities;
-	* Timestamp - time of log creation - set during log entity processing;
-	* CallContext - source code context - set during log entity processing.
-The structure should not be modified directly by the user.
-There are several methods to operate on this structure.
-
-Properties
-
-WithProperty and WithProperties methods insert new or update existing key-value properties
-of the log message entity, e.g.
+To make parsing of logger messages easier, you should use logger.Properties. Properties can be
+serialized separately and easily parsed. WithProperty and WithProperties methods insert new or
+update existing key-value properties of the log message entity, e.g.
 	logger.WithProperty("ID", 17).Info("New object created.")
 	logger.WithProperties(logger.Properties{
 		name:		"John",
@@ -101,42 +110,42 @@ There is also a special property for logging errors than can be added by WithErr
 		logger.WithErrror(err).Error("Getting things done failed.")
 	}
 
-If these methods are run on existing Entry structure, they modify and return
-the Entry structure.
 
-If these methods are run on Logger structure, they create and return new Entry structure with
-defined properties.
+Every log message entity gets CallContext during processing, containing:
 
-CallContext and Timestamp are collected automatically when completed log message entity
-is processed.
+* Path - path the source file from which a log was created;
 
-CallContext
+* File - filename of the source file;
 
-Every log message entity get source code context during processing. The context contains:
-	* Path - path the source file from which a log was created;
-	* File - filename of the source file;
-	* Line - line number;
-	* Package - package name;
-	* Type - name of type for which method was defined (in case of methods only);
-	* Function - name of function or method.
-The full context is available during processing of the log message entity, but which information
-is written to logs depends on Logger (Serializers used in Backends to be more precise).
+* Line - line number;
 
-So both collection of call context and writing it is pretty automated. However there are
-situations when some kind of auxiliary helper functions log an error. In such case
-the code author's intention is probably to log place where helper function is used
-instead of specific line in helper function. In such case an IncDepth method can be used to
-change the default call stack depth and get call context of the caller, e.g.
-	func helper(check Stuff) {
-		if !check.OK() {
-			logger.IncDep(1).Error("Stuff check failed v1.")
-			logger.Error("Stuff check failed v2.")	// call context of v2 will point here.
+* Package - package name;
+
+* Type - name of type for which method was defined (in case of methods only);
+
+* Function - name of function or method.
+
+It is up to Serializer used in Backends (described further down this document) which information is
+written to logs.
+
+There are situations when some kind of auxiliary helper functions log an error. In such case the
+code author's intention is probably to log place where helper function is used instead of specific
+line in helper function. In such case an IncDepth method can be used to change the default call
+stack depth and get call context of the caller, e.g.
+	func exitOnErr(msg string, err error) {
+		if err!=nil {
+			logger.IncDep(1).WithError(err).Error("msg")
+			os.Exit(1)
 		}
 	}
-	func stuffMaker() {
-		stuff := NewStuff()
-		helper(stuff)	// call context of v1 will point here.
-	}
+	func main() {
+		obj,err:= NewObj()
+		exitOnErr("Failed to create object.", err)	// call context will point here
+	}												//rather than to exitOnErr function.
+
+If methods from this paragraph are run on existing Entry structure, they modify and return the Entry
+structure.If they are run on Logger structure, they create and return new Entry structure with
+defined properties.
 
 Processing log messages
 
@@ -144,39 +153,46 @@ Every log message entity is processed after calling one of Log, Logf, Debug, Deb
 Infof, ... logging functions.
 
 Processing of an entity consist of following steps:
- 
-1) Verification of threshold test. If test fails, the log entity is dropped.
 
-2) Completing Entry structure by adding timestamp and call context information.
+1) Verification of threshold. If it fails, the log entity is dropped.
+
+2) Add timestamp and call context.
 
 3) Passing an Entry structure to every Backend registered in Logger and continuing processing
 in every backend.
 
 Backends
 
-Backends are the customizable parts of the logger that allow filtering logs, defining the way
+Backends are customizable parts of logger that allow filtering logs, defining the way
 they are formatted and choosing the destination where they are finally written.
 
-Logger can have multiple backends registered. Every backend works independently, so e.g.
-filtering an Entry by one of them does not affect processing the log entitty in another one.
-Backends are identified with name (string), so adding a backend with name already used, will
-replace the old one.
+Logger can have multiple backends registered. Every backend works independently, so e.g.  filtering
+an Entry by one of them does not affect processing the log entitty in another one.  Backends are
+identified with name (string), so adding new backend with a name that is already used, will override
+old backend.
 
 Multiple backends with different filters can be used for logging specific entities into additional
 files, logs, network locations, etc. for example all security logs or network logs containing
 some special property can be passed to specific files.
 
 Backends can be dynamically added or removed from Logger with following functions:
-	* AddBackend - add (or replace) a single backend;
-	* RemoveBackend - remove a single backend;
-	* RemoveAllBackends - clear all backends collection from Logger.
+
+* AddBackend - add (or replace) a single backend;
+
+* RemoveBackend - remove a single backend;
+
+* RemoveAllBackends - clear all backends collection from Logger.
+
 After removing all backends, you should probably add at least one backend, as it will make
 no sense to have no backends, as you logger won't be able to log anything at all.
 
 Every backend consists of 3 elements:
-	* Filter - for choosing which entities should be handled by the Backend;
-	* Serializer - for marshaling Entry structure into []byte;
-	* Writer - for saving/sending entities.
+
+* Filter - for choosing which entities should be handled by the Backend;
+
+* Serializer - for marshaling Entry structure into []byte;
+
+* Writer - for saving/sending entities.
 
 Filter
 
@@ -194,9 +210,12 @@ It is an interface that requires implementation of a single method:
 	Serialize(*Entry) ([]byte, error)
 
 There are 2 example implementations of this interface:
-	* SerializerJSON - that uses JSON format for Entry serialization;
-	* SerializerText - that is intended to produce human-readable from of logs for consoles
-		or log files.
+
+* SerializerJSON - that uses JSON format for Entry serialization;
+
+* SerializerText - that is intended to produce human-readable from of logs for consoles
+or log files.
+
 Both of them are configurable. Please see fields' descriptions of structures defining them
 for details.
 
@@ -209,34 +228,14 @@ which is very similiar to io.Writer interface, but requiring a log level as ther
 destinations, e.g. syslog that require this information.
 
 There are 3 example implementations of this interface:
-	* WriterFile - that saves log entities into files;
-	* WriterStderr - that prints logs to standard error output;
-	* WriterSyslog - that logs to system logger using log/syslog package.
+
+* WriterFile - that saves log entities into files;
+
+* WriterStderr - that prints logs to standard error output;
+
+* WriterSyslog - that logs to system logger using log/syslog package.
+
 See their constructors for more customized usage.
 
-Default
-
-To make usage of logger package easier, the default Logger is already initialized and
-ready to be used in simple scenarios. It has threshold set to Info level, one backend
-with FilterPassAll, SerializerText and WriterStderr.
-
-All global package functions operate on the default Logger.
-
-The default logger can be set to customized solution with SetDefault function, e.g.
-	func() initializeMyLogger(filename string, level string) error {
-		lvl, err := StringToLevel(level)
-		if err != nil {
-			return err
-		}
-		myLogger := NewLogger()
-		myLogger.SetThreshold(lvl)
-		myLogger.AddBackend("myBackend", Backend{
-			Filter:     NewFilterPassAll(),
-			Serializer: NewSerializerJSON(),
-			Writer:     NewWriterFile(filename, 0444),
-		})
-		logger.SetDefault(myLogger)
-		return nil
-	}
 */
 package logger
