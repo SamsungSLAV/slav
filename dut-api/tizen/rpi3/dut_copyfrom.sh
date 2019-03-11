19fa96a682a929041d93f27074b43338449ac491
Alexander Mazuruk <a.mazuruk@samsung.com>
2019-03-11 21:26:41 +0100

    Add dut-api documentation
    This is the initial version of dut-api documentation.
    
    It needs review and testing.
    There is an issue with RaspberryPi firmware making SD-MUX capability of
    MuxPi unusable... Possible solutions:
    - document using SDwire instead of MuxPi's SD multiplexer.
    - use less popular board without this issue (Odroid U3)
    - use other OS (would need to produce such dut scripts)
    
    This still needs to be divded and probably moved somewhere so I'm
    leaving it as one commit.
    
    Signed-off-by: Alexander Mazuruk <a.mazuruk@samsung.com>
    Change-Id: Ib0e01da47e1a9b25da07e4fd1a07e4d207872460

---
+++ dut-api/tizen/rpi3/dut_copyfrom.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+STDOUT_FILE=/tmp/weles_cft_stdout.txt
+STDERR_FILE=/tmp/weles_cft_stderr.txt
+
+rm $STDOUT_FILE $STDERR_FILE
+
+(((sdb pull $1 $2 | tee $STDOUT_FILE) 3>&1 1>&2 2>&3 | tee $STDERR_FILE) 3>&1 1>&2 2>&3) 
+
+#if [ -s $STDERR_FILE ]; then
+#	exit 1
+#else
+exit 0
+#fi
